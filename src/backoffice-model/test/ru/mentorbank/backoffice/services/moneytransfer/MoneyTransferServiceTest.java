865bbf4df78d1e11748ee3f975750e11215cc5d7
John Johnson <script08@gmail.com>
2011-12-06 19:22:43 +0400

    saveOperaion test added

--- src/backoffice-model/test/ru/mentorbank/backoffice/services/moneytransfer/MoneyTransferServiceTest.java
+++ src/backoffice-model/test/ru/mentorbank/backoffice/services/moneytransfer/MoneyTransferServiceTest.java
@@ -6,11 +6,15 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.mockito.Mockito.any;
 import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.doNothing;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import ru.mentorbank.backoffice.dao.OperationDao;
+import ru.mentorbank.backoffice.dao.exception.OperationDaoException;
+import ru.mentorbank.backoffice.model.Operation;
 import ru.mentorbank.backoffice.model.stoplist.JuridicalStopListRequest;
 import ru.mentorbank.backoffice.model.stoplist.PhysicalStopListRequest;
 import ru.mentorbank.backoffice.model.stoplist.StopListInfo;
@@ -24,6 +28,7 @@ import ru.mentorbank.backoffice.services.moneytransfer.exceptions.TransferExcept
 import ru.mentorbank.backoffice.services.stoplist.StopListService;
 import ru.mentorbank.backoffice.services.stoplist.StopListServiceStub;
 import ru.mentorbank.backoffice.test.AbstractSpringTest;
+import ru.mentorbank.backoffice.dao.stub.OperationDaoStub;
 
 public class MoneyTransferServiceTest extends AbstractSpringTest {
 
@@ -31,16 +36,18 @@ public class MoneyTransferServiceTest extends AbstractSpringTest {
 	private MoneyTransferServiceBean moneyTransferService;
 	private StopListService mockedStopListService;
 	private AccountService mockedAccountService;
+	private OperationDaoStub mockedOperationDao;
 	
 	@Before
 	public void setUp() {
 		mockedStopListService = mock(StopListServiceStub.class);
 		mockedAccountService = mock(AccountServiceBean.class);
+		mockedOperationDao = mock(OperationDaoStub.class);
 		 
 	}
 
 	@Test
-	public void transfer() throws TransferException {
+	public void transfer() throws TransferException, OperationDaoException {
 		//fail("not implemented yet");
 		// TODO: Необходимо протестировать, что для хорошего перевода всё
 		// работает и вызываются все необходимые методы сервисов
@@ -50,6 +57,7 @@ public class MoneyTransferServiceTest extends AbstractSpringTest {
 		 
 		 JuridicalAccountInfo srcAccountInfo = new JuridicalAccountInfo();
 		 PhysicalAccountInfo dstAccountInfo = new PhysicalAccountInfo();
+		 
 		 
 		 srcAccountInfo.setAccountNumber("111111111111111");
 		 srcAccountInfo.setInn(StopListServiceStub.INN_FOR_OK_STATUS);
@@ -62,6 +70,7 @@ public class MoneyTransferServiceTest extends AbstractSpringTest {
 		 
 		 moneyTransferService.setAccountService(mockedAccountService);
 		 moneyTransferService.setStopListService(mockedStopListService);
+		 moneyTransferService.setOperationDao(mockedOperationDao);
 		 
 		 when(mockedAccountService.verifyBalance(srcAccountInfo)).thenReturn(true);
 		 StopListInfo okStopListInfo = new StopListInfo();
@@ -69,9 +78,11 @@ public class MoneyTransferServiceTest extends AbstractSpringTest {
 		 when(mockedStopListService.getJuridicalStopListInfo(any(JuridicalStopListRequest.class))).thenReturn(okStopListInfo);
 		 when(mockedStopListService.getPhysicalStopListInfo(any(PhysicalStopListRequest.class))).thenReturn(okStopListInfo);
 		 
+		 
 		 moneyTransferService.transfer(request);
 		 verify(mockedStopListService).getJuridicalStopListInfo(any(JuridicalStopListRequest.class));
 		 verify(mockedAccountService).verifyBalance(srcAccountInfo);
+		 verify(mockedOperationDao).saveOperation(any(Operation.class));
 		 
 	}
 }
